d4579bf27f6b0701fc8d4cb18ac7045393e6a31d
Spartak Maznytskyi <109909363+Spartakm67@users.noreply.github.com>
2023-02-27 18:05:36 +0200

    Learning

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,8 +5,9 @@ import { Foreva } from './Toggle.styled';
 export const App = () => {
   return (
     <Container>
-      TERRA FOOD   
+      TERRA FOOD
       <Toggle ><Foreva>FOREVA</Foreva></Toggle >
+      
     </Container >
   );
 };
@@ -32,4 +33,60 @@ class Toggle extends Component {
   }
 }
 
-// style={ToggleStyle}
+// class Toggle extends Component {
+//   state = { isOpen: false };
+
+//   toggle = () => {
+//     this.setState(state => ({ isOpen: !state.isOpen }));
+//   };
+
+//   render() {
+//     const { isOpen } = this.state;
+//     const { children } = this.props;
+
+//     return (
+//       <div>
+//         <button onClick={this.toggle}>{isOpen ? "Hide" : "Show"}</button>
+//         {isOpen && children}
+//       </div>
+//     );
+//   }
+// }
+//===============================================================//
+
+// export const App = () => {
+//   return (
+//     <Container>
+//       <Toggle ></Toggle >
+//     </Container >
+//   );
+// };
+
+// const Button = ({ changeMessage, label }) => (
+//   <button type="button" onClick={changeMessage}>
+//     {label}
+//   </button>
+// );
+
+// class Toggle extends Component {
+//   state = {
+//     message: new Date().toLocaleTimeString(),
+//   };
+
+//   // Метод, який будемо передавати в Button для виклику під час кліку
+//   updateMessage = evt => {
+//     console.log(evt); // Доступний об'єкт події
+//     this.setState({
+//       message: new Date().toLocaleTimeString(),
+//     });
+//   };
+
+//   render() {
+//     return (
+//       <>
+//         <span>{this.state.message}</span>
+//         <Button label="Change message" changeMessage={this.updateMessage} />
+//       </>
+//     );
+//   }
+// }
