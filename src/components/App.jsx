e4ea5c6a3b166823c00b0e218a4af9c366c77157
Spartak Maznytskyi <109909363+Spartakm67@users.noreply.github.com>
2023-02-27 16:45:22 +0200

    TF

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,35 @@
+import { Component } from 'react';
+import { Container } from './App.styled';
+import { Foreva } from './Toggle.styled';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container>
+      TERRA FOOD   
+      <Toggle ><Foreva>FOREVA</Foreva></Toggle >
+    </Container >
   );
 };
+
+class Toggle extends Component {
+  state = { isOpen: false };
+
+  show = () => this.setState({ isOpen: true });
+
+  hide = () => this.setState({ isOpen: false });
+
+  render() {
+    const { isOpen } = this.state;
+    const { children } = this.props;
+
+    return (
+      <>
+        <button onClick={this.show}>Show</button>
+        <button onClick={this.hide}>Hide</button>
+        {isOpen && children}
+      </>
+    );
+  }
+}
+
+// style={ToggleStyle}
---
+++ src/components/Toggle.styled.jsx
@@ -0,0 +1,6 @@
+import styled from '@emotion/styled';
+
+export const Foreva = styled.span`
+color: red;
+margin-left: 10px;
+`;
