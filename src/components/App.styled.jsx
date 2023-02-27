1094e5f271b8697f553bc375e0e92fca77a2014b
Spartak Maznytskyi <109909363+Spartakm67@users.noreply.github.com>
2023-02-27 22:41:02 +0200

    Task 1 V.3

--- src/components/App.styled.jsx
+++ src/components/App.styled.jsx
@@ -3,6 +3,8 @@ import styled from '@emotion/styled'
 export const Container = styled.div`
     height: 100vh;
     display: flex;
+    flex-direction: column;
+    // gap: 30px;
     justify-content: center;
     align-items: center;
     font-size: 40px;
